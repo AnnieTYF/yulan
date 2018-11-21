f83cb617b3fd10c0c57f561837f777ba10c5aa15
AnnieTYF <980315703@qq.com>
2018-11-21 22:03:11 +0800

    初始化

---
+++ src/main/java/com/yulan/pojo/User.java
@@ -0,0 +1,31 @@
+package com.yulan.pojo;
+
+public class User {
+
+    private String customerId;
+    private String customerName;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "customerId='" + customerId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                '}';
+    }
+}
