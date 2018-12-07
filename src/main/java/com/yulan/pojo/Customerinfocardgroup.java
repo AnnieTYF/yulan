bdd151d540a97f84b87dd7670ca33d816c5f23b9
0jinyun <1219633494@qq.com>
2018-12-07 19:59:07 +0800

    整合用户表

--- src/main/java/com/yulan/pojo/Customerinfocardgroup.java
+++ src/main/java/com/yulan/pojo/Customerinfocardgroup.java
@@ -2,49 +2,54 @@ package com.yulan.pojo;
 
 import java.sql.Date;
 
-public class CustomerInfoCardGroup {
+public class Customerinfocardgroup {
 
-    private String ID;
-    private String areaCode;
-    private String areaDistrict;
-    private String customerType;
+    private String Id;
+    private String area_code;
+    private String area_district;
+    private String customer_type;
     private String descp;
     private Date createts;
-    private Integer cardCount;
-    private Integer contractYear;
+    private Integer cardcount;
+    private Integer contractyear;
     private Integer deleted;
-    private Integer erpCopyed;
+    private Integer erpcopyed;
 
-    public String getID() {
-        return ID;
+    private String area_codeName;//市场名
+    private String area_districtName;//区域名
+    private String customer_typeName;
+
+
+    public String getId() {
+        return Id;
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
+    public void setId(String id) {
+        Id = id;
     }
 
-    public String getAreaCode() {
-        return areaCode;
+    public String getArea_code() {
+        return area_code;
     }
 
-    public void setAreaCode(String areaCode) {
-        this.areaCode = areaCode;
+    public void setArea_code(String area_code) {
+        this.area_code = area_code;
     }
 
-    public String getAreaDistrict() {
-        return areaDistrict;
+    public String getArea_district() {
+        return area_district;
     }
 
-    public void setAreaDistrict(String areaDistrict) {
-        this.areaDistrict = areaDistrict;
+    public void setArea_district(String area_district) {
+        this.area_district = area_district;
     }
 
-    public String getCustomerType() {
-        return customerType;
+    public String getCustomer_type() {
+        return customer_type;
     }
 
-    public void setCustomerType(String customerType) {
-        this.customerType = customerType;
+    public void setCustomer_type(String customer_type) {
+        this.customer_type = customer_type;
     }
 
     public String getDescp() {
@@ -63,20 +68,20 @@ public class CustomerInfoCardGroup {
         this.createts = createts;
     }
 
-    public Integer getCardCount() {
-        return cardCount;
+    public Integer getCardcount() {
+        return cardcount;
     }
 
-    public void setCardCount(Integer cardCount) {
-        this.cardCount = cardCount;
+    public void setCardcount(Integer cardcount) {
+        this.cardcount = cardcount;
     }
 
-    public Integer getContractYear() {
-        return contractYear;
+    public Integer getContractyear() {
+        return contractyear;
     }
 
-    public void setContractYear(Integer contractYear) {
-        this.contractYear = contractYear;
+    public void setContractyear(Integer contractyear) {
+        this.contractyear = contractyear;
     }
 
     public Integer getDeleted() {
@@ -87,11 +92,54 @@ public class CustomerInfoCardGroup {
         this.deleted = deleted;
     }
 
-    public Integer getErpCopyed() {
-        return erpCopyed;
+    public Integer getErpcopyed() {
+        return erpcopyed;
     }
 
-    public void setErpCopyed(Integer erpCopyed) {
-        this.erpCopyed = erpCopyed;
+    public void setErpcopyed(Integer erpcopyed) {
+        this.erpcopyed = erpcopyed;
+    }
+
+    public String getArea_codeName() {
+        return area_codeName;
+    }
+
+    public void setArea_codeName(String area_codeName) {
+        this.area_codeName = area_codeName;
+    }
+
+    public String getArea_districtName() {
+        return area_districtName;
+    }
+
+    public void setArea_districtName(String area_districtName) {
+        this.area_districtName = area_districtName;
+    }
+
+    public String getCustomer_typeName() {
+        return customer_typeName;
+    }
+
+    public void setCustomer_typeName(String customer_typeName) {
+        this.customer_typeName = customer_typeName;
+    }
+
+    public Customerinfocardgroup() {
+    }
+
+    public Customerinfocardgroup(String id, String area_code, String area_district, String customer_type, String descp, Date createts, Integer cardcount, Integer contractyear, Integer deleted, Integer erpcopyed, String area_codeName, String area_districtName, String customer_typeName) {
+        Id = id;
+        this.area_code = area_code;
+        this.area_district = area_district;
+        this.customer_type = customer_type;
+        this.descp = descp;
+        this.createts = createts;
+        this.cardcount = cardcount;
+        this.contractyear = contractyear;
+        this.deleted = deleted;
+        this.erpcopyed = erpcopyed;
+        this.area_codeName = area_codeName;
+        this.area_districtName = area_districtName;
+        this.customer_typeName = customer_typeName;
     }
 }
